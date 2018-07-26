93645863b1c914a38106cd6cec0ed1a684e48d31
pengfei.cheng <jia007_chengpengfei@tencent.com>
2018-07-26 18:31:19 +0800

    111

---
+++ src/main/java/com/example/secound/SecoundApplication.java
@@ -0,0 +1,12 @@
+package com.example.secound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecoundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecoundApplication.class, args);
+	}
+}
