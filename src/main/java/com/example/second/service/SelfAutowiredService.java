3b43f4bc87b870ff04e0a047bccf6bd74410c70c
pengfei.cheng <jia007_chengpengfei@tencent.com>
2018-07-27 17:26:37 +0800

    提交

---
+++ src/main/java/com/example/second/service/SelfAutowiredService.java
@@ -0,0 +1,14 @@
+package com.example.second.service;
+
+/**
+ * @Author: pengfei.cheng
+ * @Date: 2018/7/27 下午3:24
+ * @Company: 易宝支付(YeePay)
+ */
+public interface SelfAutowiredService {
+
+
+    void sout(String path);
+
+    void printf(String path);
+}
--- src/test/java/com/example/second/SecondApplicationTests.java
+++ src/test/java/com/example/second/SecondApplicationTests.java
@@ -1,13 +1,18 @@
 package com.example.second;
 
+import com.example.second.service.SelfAutowiredService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SecondApplicationTests {
+
+	@Autowired
+	private SelfAutowiredService selfAutowiredService;
 
 	@Test
 	public void contextLoads() {
