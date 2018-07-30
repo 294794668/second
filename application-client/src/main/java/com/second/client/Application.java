e9778aeb21d25e9809d390074c7ebf2908e8d97d
pengfei.cheng <jia007_chengpengfei@tencent.com>
2018-07-30 15:05:08 +0800

    提交-moudel 改造

---
+++ application-client/src/main/java/com/second/client/Application.java
@@ -0,0 +1,16 @@
+package com.second.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: pengfei.cheng
+ * @Date: 2018/7/30 下午2:52
+ * @Company: 易宝支付(YeePay)
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
--- eureka-service/src/test/java/com/example/second/SecondApplicationTests.java
+++ eureka-service/src/test/java/com/example/second/SecondApplicationTests.java
@@ -1,6 +1,5 @@
 package com.example.second;
 
-import com.example.second.service.SelfAutowiredService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,9 +9,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SecondApplicationTests {
-
-	@Autowired
-	private SelfAutowiredService selfAutowiredService;
 
 	@Test
 	public void contextLoads() {
